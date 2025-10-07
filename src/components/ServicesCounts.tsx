ffefb4db1f233f1da4d81ff27a8135ff5d65087f
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-10-07 17:01:43 +0530

    fix

--- src/components/ServicesCounts.tsx
+++ src/components/ServicesCounts.tsx
@@ -4,9 +4,9 @@ const ServicesCounts = () => {
         <div className="bg-[#1F1F1F] font-[Manrope] text-white py-10 sm:rounded-2xl lg:flex grid flex-row grid-cols-2 xl:gap-x-10 lg:gap-x-2 sm:gap-x-4 gap-y-6 justify-evenly w-full xl:-translate-y-24 lg:-translate-y-24 sm:-translate-y-34 -translate-y-30">
             {[
                 { count: 15, sign: "+", text: "Years Of Experience" },
-                { count: 20, sign: "K+", text: "Years Of Experience" },
+                { count: 20, sign: "K+", text: "Happy Customers" },
                 { count: 25, sign: "+", text: "Areas Covered" },
-                { count: 16, sign: "+", text: "Areas Covered" },
+                { count: 16, sign: "K+", text: "Devices Repaired" },
             ].map((obj) => (
                 <DataMapper key={obj.count} count={obj.count} sign={obj.sign} text={obj.text} />
             ))}
